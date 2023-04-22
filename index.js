2e3b07377f1c02db05bb79137e4b85be75574043
Jeremy Garcia <jeremygarcia9400@gmail.com>
2023-04-22 12:40:15 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,22 +1,47 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  for (let i = 0; i < array.length-1; i++) {
+    if (array.includes(target - array[i], i+1)) {
+      return true;
+    }
+  }
+  return false;
 }
 
 /* 
-  Write the Big O time complexity of your function here
+  O(n log n)
 */
 
 /* 
-  Add your pseudocode here
+  Subtract every element from target expect for the last one.
+  Check the rest of the elements to the right if they match this difference.
+  If it does, return true.
+  If whole iteration finihes with no matches, return false.
 */
 
 /*
-  Add written explanation of your solution here
+  Create for loop that iterates every element except the last one
+  For each iteration, use .includes to see if difference of target and current element match any of the following elements.
+  If any match is found, return true.
+  If all iterations are complete and no iterations are found, return false.
 */
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
-  // add your own custom tests in here
+ console.log("Expecting: true");
+  console.log("=>", hasTargetSum([2, 8, 12, 4, 11, 7], 10));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", hasTargetSum([11, 8, 12, 4, 3, 7], 10));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", hasTargetSum([1, 8, 12, -2, 11, 7], 10));
+
+  console.log("");
+  
   console.log("Expecting: true");
   console.log("=>", hasTargetSum([3, 8, 12, 4, 11, 7], 10));
 
